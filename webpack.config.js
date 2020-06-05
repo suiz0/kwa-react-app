94313855276ce7ccd6a17cbf088d019e092c623d
cesar iduarte <ciduarte@gmail.com>
2020-06-05 14:58:53 -0500

    Change mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -40,6 +41,11 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "./src/style.css"
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: path.resolve(__dirname, 'config.json')},
+            ],
+          })
     ]
 }
