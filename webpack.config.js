56ea972541e330f1016c84546aab921aa6d3e4d5
cesar iduarte <ciduarte@gmail.com>
2020-06-09 22:45:28 -0500

    Add clean-webpack-plugin to clear output directory before each build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -35,6 +36,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html")
         }),
