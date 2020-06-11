aac3ffc0ec35a7f611efd60e2c56de7300ff6455
cesar <ciduarte@gmail.com>
2020-06-11 17:13:31 -0500

    Add comments to the component

--- src/modules/auth/components/WithAuth.tsx
+++ src/modules/auth/components/WithAuth.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AuthAPIProvider} from '../services/AuthAPI';
 
+// Registers auth service and passed down as a property
 const withAuth = (WrappedComponent) => {
     return class extends React.Component {
         constructor(props)
@@ -8,6 +9,7 @@ const withAuth = (WrappedComponent) => {
             super(props);
         }
         render() {
+            // The provider uses the auth config key to identify the resource to attach
             return (<WrappedComponent {...this.props} auth={AuthAPIProvider.create()}  />
             );
         }
